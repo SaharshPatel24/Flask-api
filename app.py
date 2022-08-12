3f1aeb209afa163c1464184ffe3a1a0e971d9b28
SaharshPatel24 <srpatel56@myseneca.ca>
2022-08-11 23:24:58 -0400

    Initialized Flask server

---
+++ app.py
@@ -0,0 +1,11 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return "Hello World From Flask Server!"
+
+@app.route("/admin")
+def admin():    
+    return " Hello from Admin Server! "    
