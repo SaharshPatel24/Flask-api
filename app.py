b6a260c3e99166a428c8e120ceb70d1a8a43924d
SaharshPatel24 <srpatel56@myseneca.ca>
2022-08-13 11:34:07 -0400

    Added the index.html

--- app.py
+++ app.py
@@ -1,11 +1,14 @@
-from flask import Flask
+from flask import Flask, render_template
+
+UPLOAD_FOLDER = 'C:/Users/ashar/OneDrive/Desktop/Sketch-Flask-App/static/uploads'
+ALLOWED_EXTENSIONS = set(['png', 'jpg', 'jpeg'])
 
 app = Flask(__name__)
+app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+app.secret_key = "secret key"
 
-@app.route("/")
-def index():
-    return "Hello World From Flask Server!"
 
-@app.route("/admin")
-def admin():    
-    return " Hello from Admin Server! "     
+@app.route('/')
+def home():
+    return render_template('index.html')
