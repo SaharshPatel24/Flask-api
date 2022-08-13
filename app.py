dcb9d9c371b162bf76bff8cfec42b0e14195812b
SaharshPatel24 <srpatel56@myseneca.ca>
2022-08-13 11:37:27 -0400

    added the allowed extension and sketching function

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import cv2
 from flask import Flask, render_template
 
 UPLOAD_FOLDER = 'C:/Users/ashar/OneDrive/Desktop/Sketch-Flask-App/static/uploads'
@@ -7,6 +8,18 @@ app = Flask(__name__)
 app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 app.secret_key = "secret key"
+
+
+def allowed_file(filename):
+    return '.' in filename and filename.rsplit('.', 1)[1] in ALLOWED_EXTENSIONS
+
+
+def make_sketch(img):
+    grayed = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    inverted = cv2.bitwise_not(grayed)
+    blurred = cv2.GaussianBlur(inverted, (19, 19), sigmaX=0, sigmaY=0)
+    final_result = cv2.divide(grayed, 255 - blurred, scale=256)
+    return final_result
 
 
 @app.route('/')
